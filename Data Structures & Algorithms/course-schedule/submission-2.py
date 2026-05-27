3f874ea7d08f2fa461af935532bb0bd865cd25bd
A Harshavardhan Reddy <119485294+Arcane-WD@users.noreply.github.com>
2026-05-27 17:21:22 +0530

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        nodes = {}
+        for b,a in prerequisites:
+            if a not in nodes:
+                nodes[a]= []
+            nodes[a].append(b)
+        def bfs(course):
+            vis = set()
+            openl = deque([course])
+            while openl:
+                curr = openl.popleft()
+                for nei in nodes[curr]:
+                    if nei in vis:
+                        return False
+                    if nei in nodes:
+                        vis.add(nei)    
+                        openl.append(nei)
+            return True
+        # print(nodes)
+        for i in range(numCourses):
+            if i in nodes:
+                if not bfs(i):
+                    # print(i)
+                    return False
+        return True
